9917d1b301dc08413934949df39a0d8dfde7d17a
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-07-29 22:08:30 -0600

    🎉 Iniciar proyecto

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace Combina2;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainPage()) { Title = "Combina2" };
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Combina2;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
