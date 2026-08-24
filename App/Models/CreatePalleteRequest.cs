318ab1d1102c461e02be85a98f002342040fd1d5
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-08-24 15:44:20 -0600

    :necktie: (app): Definir DTOs para la APP

---
+++ App/Models/CreatePalleteRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Combina2.Models
+{
+    internal class CreatePalleteRequest
+    {
+        public List<string> Colors { get; set; } = [];
+    }
+}
---
+++ App/Models/PalleteResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Combina2.Models
+{
+    internal class PalleteResponse
+    {
+        public string Harmony { get; set; } = string.Empty;
+        public List<ColorModel> Colors { get; set; } = [];
+    }
+}
