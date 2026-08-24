19e986da3e82971d39e3dc40f364303a3c04993c
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-08-24 15:48:19 -0600

    :building_construction: (app): Agregar servicio de estado compartido
    Este servicio de estado compartido es para el formulario de generar paleta a travez de una imagen

---
+++ App/Components/Pages/Generate/ByImage/GBI_StateMachine.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Combina2.Components.Pages.Generate.ByImage
+{
+    /// <summary>
+    /// Servicio para manejar estado del componente
+    /// El servicio almacena el flujo del formulario
+    /// </summary>
+    public class GBI_StateMachine
+    {
+
+        /// <summary>
+        /// Imagen a usar para generar la paleta de colores
+        /// </summary>
+        public byte[]? Image { get; private set; }
+
+        public void SetImage(byte[] image)
+        {
+            Image = image;
+            NotifyStateChanged();
+        }
+
+        /// <summary>
+        /// Imagen Base 64 para mostrar una previsualización
+        /// </summary>
+        public string? ImagePreview { get; private set; }
+
+        public void SetImagePreview(string image)
+        {
+            ImagePreview = image;
+            NotifyStateChanged();
+        }
+
+        /// <summary>
+        /// Colores seleccionados para 
+        /// </summary>
+        public List<string> Colors { get; private set; } = [];
+
+        public void SetColors(List<string> colors)
+        {
+            Colors = colors;
+            NotifyStateChanged();
+        }
+
+        public bool Loading { get;  private set; } = false;
+
+        public void SetLoading(bool loading)
+        {
+            Loading = loading;
+            NotifyStateChanged();
+        }
+
+        public bool Loaded { get; private set; }
+
+        public void SetLoaded(bool loaded)
+        {
+            Loaded = loaded;
+            NotifyStateChanged();
+        }
+
+        public async Task Reset()
+        {
+            this.Image = null;
+            this.Colors = [];
+            this.Loading = false;
+            this.Loaded = false;
+            NotifyStateChanged();
+        }
+
+        public event Action? OnChange;
+        private void NotifyStateChanged() => OnChange?.Invoke();
+    }
+}
---
+++ App/Components/Pages/Generate/ByImage/InjectGenerateByImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Combina2.Components.Pages.Generate.ByImage
+{
+    internal static class InjectGenerateByImage
+    {
+        /// <summary>
+        /// Inyectar servicios para la vista Generate By Image
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddGBI_Services(this IServiceCollection services)
+        {
+            // Inyectar servicio state machine
+            services.AddSingleton<GBI_StateMachine>();
+
+            return services;
+        }
+    }
+}
