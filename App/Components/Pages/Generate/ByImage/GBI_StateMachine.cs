244f285404e030709026a9efbf771d252aedceef
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-08-26 22:33:05 -0600

    Mejorar maquina de estados

--- App/Components/Pages/Generate/ByImage/GBI_StateMachine.cs
+++ App/Components/Pages/Generate/ByImage/GBI_StateMachine.cs
@@ -1,76 +1,59 @@
-﻿using System;
+﻿using Combina2.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Combina2.Components.Pages.Generate.ByImage
 {
+    public sealed class FormState
+    {
+        public bool Loading { get; set; } = false;
+        public bool Loaded { get; set; } = false;
+        public byte[]? Image { get; set; } = null;
+        public string ImagePreview { get; set; } = string.Empty;
+
+        public void Reset()
+        {
+            Loading = false;
+            Loaded = false;
+            Image = null;
+            ImagePreview = string.Empty;
+        }
+    }
+
+    public sealed class ResponseState
+    {
+        public bool Loading { get; set; } = false;
+        public bool Loaded { get; set; } = false;
+
+        public PalleteResponse? Response { get; set; } = null;
+
+        public void Reset()
+        {
+            Loading = false;
+            Loaded = false;
+            Response = null;
+        }
+    }
+
+
     /// <summary>
     /// Servicio para manejar estado del componente
     /// El servicio almacena el flujo del formulario
     /// </summary>
     public class GBI_StateMachine
     {
-
-        /// <summary>
-        /// Imagen a usar para generar la paleta de colores
-        /// </summary>
-        public byte[]? Image { get; private set; }
-
-        public void SetImage(byte[]? image)
-        {
-            Image = image;
-            NotifyStateChanged();
-        }
-
-        /// <summary>
-        /// Imagen Base 64 para mostrar una previsualización
-        /// </summary>
-        public string? ImagePreview { get; private set; }
-
-        public void SetImagePreview(string image)
-        {
-            ImagePreview = image;
-            NotifyStateChanged();
-        }
-
-        /// <summary>
-        /// Colores seleccionados para 
-        /// </summary>
-        public List<string> Colors { get; private set; } = [];
-
-        public void SetColors(List<string> colors)
-        {
-            Colors = colors;
-            NotifyStateChanged();
-        }
-
-        public bool Loading { get;  private set; } = false;
-
-        public void SetLoading(bool loading)
-        {
-            Loading = loading;
-            NotifyStateChanged();
-        }
-
-        public bool Loaded { get; private set; }
-
-        public void SetLoaded(bool loaded)
-        {
-            Loaded = loaded;
-            NotifyStateChanged();
-        }
-
+        // Estado de la imagen cargada
+        public FormState FormState { get; set; } = new FormState();
+        public ResponseState ResponseState { get; set; } = new ResponseState();
         public async Task Reset()
         {
-            this.Image = null;
-            this.ImagePreview = String.Empty;
-            this.Colors = [];
-            this.Loading = false;
-            this.Loaded = false;
+            FormState.Reset();
+            ResponseState.Reset();
             NotifyStateChanged();
         }
 
         public event Action? OnChange;
-        private void NotifyStateChanged() => OnChange?.Invoke();
+        public void NotifyStateChanged() => OnChange?.Invoke();
     }
 }
