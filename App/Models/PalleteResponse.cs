58e8594910356dc6550a8bf3d3385b116cf11994
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-08-30 14:33:06 -0600

    Agregar atributos json a la propiedad

--- App/Models/PalleteResponse.cs
+++ App/Models/PalleteResponse.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Combina2.Models
 {
     public class PalleteResponse
     {
+        [JsonPropertyName("harmony")]
         public string Harmony { get; set; } = string.Empty;
+        [JsonPropertyName("colors")]
         public List<ColorModel> Colors { get; set; } = [];
     }
 }
