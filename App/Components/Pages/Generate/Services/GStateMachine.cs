075f23cd9f91a03696f354050c611602f422140a
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-08-30 15:28:49 -0600

    :truck: (app): Crear carpeta de servicios y moverlos a esta carpeta
    Mejorar organización del modulo Generate para que el código sea mas entendible

--- App/Components/Pages/Generate/Services/GStateMachine.cs
+++ App/Components/Pages/Generate/Services/GStateMachine.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Combina2.Components.Pages.Generate;
+namespace Combina2.Components.Pages.Generate.Services;
 
 
 /// <summary>
--- App/Components/Pages/Generate/Services/InjectGenerate.cs
+++ App/Components/Pages/Generate/Services/InjectGenerate.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Combina2.Services;
 
-namespace Combina2.Components.Pages.Generate
+namespace Combina2.Components.Pages.Generate.Services
 {
     internal static class InjectGenerate
     {
