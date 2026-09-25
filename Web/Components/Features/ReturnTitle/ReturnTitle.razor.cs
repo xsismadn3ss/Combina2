d15ae7dea0b2a1237c2bacab0bf49213ecad304d
xsismadn3ss <2477carlos.artiga@gmail.com>
2026-09-25 10:22:56 -0600

    🏗️ Crear proyecto web con Blazor wasm
    Migrar proyeco a Blazor wasm para utilizar una plantilla web en vez de MAUI para desarrollar mucho más rápido

---
+++ Web/Components/Features/ReturnTitle/ReturnTitle.razor.cs
@@ -0,0 +1,10 @@
+namespace Web.Components.Features.ReturnTitle
+{
+    /// <summary>
+    /// Usa este componente para indicar el titulo de página y agregar
+    /// un botón para volver en el historial
+    /// </summary>
+    public partial class ReturnTitle
+    {
+    }
+}
